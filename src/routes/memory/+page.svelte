66eb997c3b193e4a6db13fab63c150fa233ddc38
DaemonNine <92573737+DaemonNine@users.noreply.github.com>
2023-02-01 14:11:15 +0100

    Completed memory

--- src/routes/memory/+page.svelte
+++ src/routes/memory/+page.svelte
@@ -1,43 +1,100 @@
 <script>
+  import toast, { Toaster } from "svelte-french-toast";
+  import { onMount } from "svelte";
+
   let cards = [];
-  let pairs = 4;
+  let pairs = 4; // Varibel för hur många par det finns.
   for (let index = 0; index < pairs; index++) {
     cards.push({
       id: index, // TODO: unique ids per card card
-      img: "https://picsum.photos/id/"+index.toString() +"/200/300", // TODO: unique images per card card
-      flipped: false,  // TODO: think
+      img: "https://picsum.photos/id/" + index.toString() + "/200/300", // TODO: unique images per card card
+      flipped: false, // TODO: think
       completed: false,
     });
     cards.push({
-      id: index+pairs, // TODO: unique ids per card card
-      img: "https://picsum.photos/id/"+index.toString() +"/200/300", // TODO: unique images per card card
-      flipped: false,  // TODO: think
+      id: index + pairs, // Nu skapas ett identiskt kort med ett ID som är pairs större.
+      img: "https://picsum.photos/id/" + index.toString() + "/200/300", // TODO: unique images per card card
+      flipped: false, // TODO: think
       completed: false,
     });
-     // Nu har den skapat 6 kort med olika bilder
+    // Nu har det skapats 4 par med olika bilder
   }
+
+  
+
+  function shuffle(array) {
+    let currentIndex = array.length,
+      randomIndex;
+
+    // While there remain elements to shuffle.
+    while (currentIndex != 0) {
+      // Pick a remaining element.
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+
+      // And swap it with the current element.
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex],
+        array[currentIndex],
+      ];
+    }
+
+    return array;
+  }
+ 
+  shuffle(cards);
+  
+
   let flipcount = 0;
   function flip(card) {
     // flip card over if two cards are not already flipped
     // TODO: and card is already not flipped
-    if (!card.flipped && flipcount < 2 ) {
+    if (!card.flipped && flipcount < 2) {
       // TODO: Probably do what?
-      flipcount+=1;
+      flipcount += 1;
       card.flipped = true;
       // flip the cards over after 2s from seeing both cards
       if (flipcount == 2) {
-        setTimeout(() => {
-          // flip over cards that have not been marked as "completed"
-          cards.forEach((card) => {
-            card.flipped = card.completed;
-          });
-          flipcount = 0;
-          cards = cards;
-        }, 2000);
+        // TODO: check if the flipped card is equal to the previously flipped card.
+        // if equal, set as completed
+
+        let completed = false;
+
+        // When two cards have flipped, find pair card
+        cards.forEach((temp) => {
+          // if the card is the pair card
+          if (temp.id + pairs == card.id || temp.id - pairs == card.id) {
+            // check if the pair is already flipped, if so completed true
+            if (temp.flipped) {
+              
+              temp.completed = true;
+              card.completed = true;
+              completed = true;
+            }
+          }
+        });
+        toast.promise(
+          new Promise((resolve, reject) =>
+            setTimeout(() => {
+              // flip over cards that have not been marked as "completed"
+              cards.forEach((card) => {
+                card.flipped = card.completed;
+              });
+              flipcount = 0;
+              cards = cards;
+              if (!completed) {
+                reject();
+              } else {
+                resolve();
+              }
+            }, 2000)
+          ),
+          { loading: "waiting...", success: "good job!", error: "try gain!" }
+        );
       }
       cards = cards;
     } else {
-      alert("chill");
+      toast.error("chill");
     }
   }
 </script>
@@ -61,6 +118,7 @@
     {/each}
   </div>
 </main>
+<Toaster />
 
 <style>
   main {
